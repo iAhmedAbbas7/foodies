e273f3f09d6f52f79d77e7113dded6755223e39b
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-10-22 14:58:04 +0500

    Integrated the Global Layout in my App's Routing System

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -4,6 +4,7 @@ import SignUp from "./auth/SignUp";
 import VerifyEmail from "./auth/VerifyEmail";
 import LandingPage from "./pages/LandingPage";
 import ResetPassword from "./auth/ResetPassword";
+import GlobalLayout from "./layouts/GlobalLayout";
 import ForgotPassword from "./auth/ForgotPassword";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -11,27 +12,33 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-  {
-    path: "/forgot-password",
-    element: <ForgotPassword />,
-  },
-  {
-    path: "/reset-password",
-    element: <ResetPassword />,
-  },
-  {
-    path: "/verify-Email",
-    element: <VerifyEmail />,
+    element: <GlobalLayout />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <SignUp />,
+      },
+      {
+        path: "forgot-password",
+        element: <ForgotPassword />,
+      },
+      {
+        path: "reset-password",
+        element: <ResetPassword />,
+      },
+      {
+        path: "verify-Email",
+        element: <VerifyEmail />,
+      },
+    ],
   },
 ]);
 
