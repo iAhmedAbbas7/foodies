e28dd5a9ff532768848e144a0a82f034f8e83b21
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-10-25 16:35:47 +0500

    Integrated the HomePage Component in the App Router

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -7,6 +7,7 @@ import ResetPassword from "./auth/ResetPassword";
 import GlobalLayout from "./layouts/GlobalLayout";
 import ForgotPassword from "./auth/ForgotPassword";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 // <== APP ROUTER ==>
 const appRouter = createBrowserRouter([
@@ -14,29 +15,40 @@ const appRouter = createBrowserRouter([
     path: "/",
     element: <GlobalLayout />,
     children: [
+      // LANDING PAGE ROUTE
       {
         index: true,
         element: <LandingPage />,
       },
+      // LOGIN PAGE ROUTE
       {
         path: "login",
         element: <Login />,
       },
+      // SIGNUP PAGE ROUTE
       {
         path: "signup",
         element: <SignUp />,
       },
+      // FORGOT PASSWORD PAGE ROUTE
       {
         path: "forgot-password",
         element: <ForgotPassword />,
       },
+      // RESET PASSWORD PAGE ROUTE
       {
         path: "reset-password",
         element: <ResetPassword />,
       },
+      // VERIFY EMAIL PAGE ROUTE
       {
         path: "verify-Email",
         element: <VerifyEmail />,
+      },
+      // HOME PAGE ROUTE
+      {
+        path: "home",
+        element: <HomePage />,
       },
     ],
   },
