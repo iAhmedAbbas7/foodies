d84f29cdf3a9ee0026bf4f86586a84a6abf1cc20
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-10-25 16:32:54 +0500

    Introduced a HomePage for the Project

---
+++ Client/src/pages/HomePage.tsx
@@ -0,0 +1,43 @@
+// <== IMPORTS ==>
+import { motion } from "framer-motion";
+import useTitle from "@/hooks/useTitle";
+import Navbar from "@/components/Navbar";
+
+// <== HOME PAGE COMPONENT ==>
+const HomePage = () => {
+  // USE TITLE HOOK
+  useTitle("Foodies - Home");
+  // CONTAINER ANIMATION VARIANTS
+  const containerVariants = {
+    hidden: { opacity: 0, y: 24 },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+      when: "beforeChildren",
+      staggerChildren: 0.08,
+    },
+  } as const;
+  return (
+    // HOME PAGE MAIN WRAPPER
+    <>
+      {/* HOME PAGE CONTENT WRAPPER */}
+      <section>
+        {/* MAIN CONTENT WRAPPER */}
+        <motion.section
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          {/* NAVBAR */}
+          <Navbar />
+        </motion.section>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
