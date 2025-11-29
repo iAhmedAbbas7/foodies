8e003633a2d59492075bef485aa208e5ca2d8c77
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-11-30 00:02:54 +0500

    Removed getDirName.js File to Streamline Server Resources and Improve Project Organization

--- Server/utils/getDirName.js
+++
@@ -1,7 +0,0 @@
-// <= IMPORTS =>
-import path from "path";
-import { fileURLToPath } from "url";
-// <= NODE JS PATH HELPER FUNCTION =>
-export function getDirName(importMetaUrl) {
-  return path.dirname(fileURLToPath(importMetaUrl));
-}
