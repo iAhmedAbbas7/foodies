57c96ac3f9b713b1ccb6f2bd8841763707080eb6
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-11-30 00:33:00 +0500

    Enhance Vite Configuration: Add Structured Comments for Plugins and Resolve Settings, including Alias for Source Directory

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -4,10 +4,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// <== VITE CONFIGURATION ==>
 export default defineConfig({
+  // <== PLUGINS ==>
   plugins: [react(), tailwindcss()],
+  // <== RESOLVE ==>
   resolve: {
+    // <== ALIAS ==>
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
