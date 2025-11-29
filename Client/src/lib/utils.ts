a367721f6a38620d392ab1ffa0c00b3c696a7561
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-11-30 00:35:35 +0500

    Refactor Utility Function: Add Structured Comments for Improved Readability and Maintainability in utils.ts

--- Client/src/lib/utils.ts
+++ Client/src/lib/utils.ts
@@ -2,6 +2,8 @@
 import { twMerge } from "tailwind-merge";
 import { clsx, type ClassValue } from "clsx";
 
+// <== UTILITY FUNCTION TO MERGE CLASSNAMES ==>
 export function cn(...inputs: ClassValue[]) {
+  // MERGE CLASSNAMES USING TAILWIND-MERGE
   return twMerge(clsx(inputs));
 }
