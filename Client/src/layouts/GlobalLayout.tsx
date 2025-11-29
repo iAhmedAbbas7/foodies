4eff2b054717b9c43011fa6ce23e122d7cb90dbb
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-11-30 00:35:27 +0500

    Refactor Global Layout Component: Update Navbar and Footer Visibility Logic Based on Current Route for Improved User Experience

--- Client/src/layouts/GlobalLayout.tsx
+++ Client/src/layouts/GlobalLayout.tsx
@@ -2,22 +2,36 @@
 import { type ReactElement } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NetworkStatusWatcher from "../global/NetworkStatusWatcher";
 
 // <== GLOBAL LAYOUT COMPONENT ==>
 const GlobalLayout = (): ReactElement => {
+  // CURRENT LOCATION
+  const location = useLocation();
+  // PAGES ROUTES (FROM PAGES FOLDER)
+  const pagesRoutes = [
+    "/",
+    "/account",
+    "/stores",
+    "/menu",
+    "/track-order",
+    "/cart",
+  ];
+  // CHECK IF CURRENT ROUTE IS A PAGE ROUTE
+  const isPageRoute = pagesRoutes.includes(location.pathname);
+
   return (
     // MAIN GLOBAL LAYOUT WRAPPER
     <>
-      {/* NAVBAR - AVAILABLE ON ALL ROUTES */}
-      <Navbar />
+      {/* NAVBAR - AVAILABLE ONLY ON PAGES ROUTES */}
+      {isPageRoute && <Navbar />}
       {/* ROUTE OUTLET - RENDERS CURRENT ROUTE COMPONENT */}
-      <div className="min-h-screen pb-20 md:pb-16">
+      <div className={isPageRoute ? "min-h-screen pb-20 md:pb-16" : ""}>
         <Outlet />
       </div>
-      {/* FOOTER - AVAILABLE ON ALL ROUTES, VISIBLE ON SMALL DEVICES */}
-      <Footer />
+      {/* FOOTER - AVAILABLE ONLY ON PAGES ROUTES, VISIBLE ON SMALL DEVICES */}
+      {isPageRoute && <Footer />}
       {/* GLOBAL NETWORK STATUS WATCHER - WORKS ON ALL ROUTES AND IS ALWAYS PRESENT */}
       <NetworkStatusWatcher />
     </>
