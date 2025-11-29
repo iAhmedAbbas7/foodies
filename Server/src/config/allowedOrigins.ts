b6c3b1354dbba2c429b2d4530bcc126a19bb1291
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-11-30 00:03:37 +0500

    Add allowedOrigins.ts for CORS Configuration

---
+++ Server/src/config/allowedOrigins.ts
@@ -0,0 +1,8 @@
+// <== ALLOWED ORIGINS FOR CORS ==>
+const allowedOrigins: string[] = [
+  // FRONTEND LOCALHOST
+  "http://localhost:5173",
+  "http://localhost:5174",
+];
+
+export default allowedOrigins;
